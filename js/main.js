aab95fd4f872aec9f72a8247aedab0c7d9f571a5
Fahim Anzam Dip <dfahimanzam@gmail.com>
2020-08-02 00:55:25 +0600

    Tweaks

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
 		this.querySelectorAll('.layer').forEach(layer => {
 			const speed = layer.getAttribute('data-speed');
 
-			const x = (window.innerWidth - e.pageX*speed)/300;
-			const y = (window.innerHeight - e.pageY*speed)/300;
+			const x = (window.innerWidth - e.pageX*speed)/200;
+			const y = (window.innerHeight - e.pageY*speed)/250;
 
 			layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
 		});
